888ff6fc6a7725ffc58f4bfb3fd67b4b66503d6d
Gilbert Ardila <gilbertferney@gmail.com>
2022-01-18 11:54:59 -0500

    creamos la función para copiar el texto y para desencriptar

--- src/js/encriptar.js
+++ src/js/encriptar.js
@@ -3,6 +3,7 @@ const GettingOutWord = document.getElementById("GettingOutWord");
 var encriptar=document.getElementById("encriptar");
 var desencriptar=document.getElementById("desencriptar");
 var copiar=document.getElementById("copiar");
+/* ---------------------------Creamos función para encriptar--------------------------------------- */
 
 encriptar.addEventListener("click",function(event){
     event.preventDefault();
@@ -44,13 +45,65 @@ encriptar.addEventListener("click",function(event){
 
         }
 
-
     }
 
    //convertimos el array en string y eliminamos las comas
-    console.log(cripto.toLocaleString().replace(/,/g,""));
     var salida=cripto.toLocaleString().replace(/,/g,"");
-    console.log(salida);
+    GettingOutWord.value="";
     GettingOutWord.value+=salida;
 
 })
+
+/* ---------------------------Creamos función para el copy del botón--------------------------------------- */
+copiar.addEventListener("click",function(event){
+    event.preventDefault();
+    //limpiamos el campo de ingreso
+    GettingWord.value="";
+    //seleccionamos el texto
+    GettingOutWord.select();
+    //copiamos la selección
+    document.execCommand("copy");
+
+});
+/* ---------------------------Creamos función para desencriptar--------------------------------------- */
+desencriptar.addEventListener("click",function(event) {
+    event.preventDefault();
+    var palabra = gettingWord.value;
+    //convertimos ese valor en array para poder recorrerlo
+    var cripto = Array.from(palabra);
+    var reemplazo;
+
+    for(var i=0;i<palabra.length;i++) {
+        if (palabra.includes("ai")) {
+            reemplazo = palabra.replace("ai", "a");
+            palabra = reemplazo;
+
+        }
+        else if(palabra.includes("enter")){
+            reemplazo=palabra.replace("enter", "e");
+            palabra = reemplazo;
+
+        }
+
+        else if(palabra.includes("imes")){
+            reemplazo=palabra.replace("imes", "i");
+            palabra = reemplazo;
+
+        }
+        else if(palabra.includes("ober")){
+            reemplazo=palabra.replace("ober", "o");
+            palabra = reemplazo;
+
+        }
+        else if(palabra.includes("ufat")){
+            reemplazo=palabra.replace("ufat", "u");
+            palabra = reemplazo;
+            
+        }
+
+    }
+    GettingOutWord.value="";
+    GettingOutWord.value+=palabra;
+
+
+});
